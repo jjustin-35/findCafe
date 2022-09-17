e1ba92c7da166dd0db807922f7f76b1070f2c531
jjustin-35 <85622727+jjustin-35@users.noreply.github.com>
2022-09-17 20:53:43 +0800

    add footer link

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,30 +1,43 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { useGlobal } from '../context/GlobalProvider';
 
 export const Footer = () => {
-  const { token } = useGlobal().auth;
-  // const { profile } = loginState;
-  // const { isLogin } = profile;
-  const isLogin = true;
+  const { token, setToken } = useGlobal().auth;
+  const { setProfile } = useGlobal().userInfo;
+  const { setSearch } = useGlobal().searchState;
+  const { setErr } = useGlobal().errState;
   
   const localUrl = process.env.PUBLIC_URL;
+
+  const onLogout = (e) => {
+    e.preventDefault();
+    localStorage.removeItem('token');
+    localStorage.removeItem('profile');
+
+    setToken(null);
+    setProfile(null);
+  }
+  const navToSearch = () => {
+    setSearch({})
+  }
   return (
       <footer className='bg-primary text-white pt-4 pb-3'>
           <div className="container">
               <div className="d-flex justify-content-between align-items-md-center mb-md-5 mb-4">
                   {
-                      isLogin ? (
+                      token ? (
                           <ul className="nav flex-wrap">
                               <li className='footer__item'><a href="" className='nav-link text-white ps-0'>Home</a></li>
-                              <li className='footer__item'><a href="" className='nav-link text-white ps-0 ps-md-0-5'>Search</a></li>
-                              <li className='footer__item'><a href="" className='nav-link text-white ps-0 ps-md-0-5'>Logout</a></li>
-                              <li className='footer__item'><a href="" className='nav-link text-white ps-0 ps-md-0-5'>My account</a></li>
+                              <li className='footer__item'><Link to="/search" className='nav-link text-white ps-0 ps-md-0-5' onClick={navToSearch}>Search</Link></li>
+                              <li className='footer__item'><a href="" className='nav-link text-white ps-0 ps-md-0-5'onClick={onLogout}>Logout</a></li>
+                              <li className='footer__item'><Link to="/profile" className='nav-link text-white ps-0 ps-md-0-5'>My account</Link></li>
                         </ul>
                       ) : (
                         <ul className="nav">
                             <li className='footer__item'><a href="" className='nav-link text-white ps-0'>Home</a></li>
-                            <li className='footer__item'><a href="" className='nav-link text-white ps-0 ps-md-0-5'>Search</a></li>
-                            <li className='footer__item'><a href="" className='nav-link text-white ps-0 ps-md-0-5'>Login</a></li>
+                            <li className='footer__item'><Link to="/search" className='nav-link text-white ps-0 ps-md-0-5' onClick={navToSearch}>Search</Link></li>
+                            <li className='footer__item'><Link to="/login" className='nav-link text-white ps-0 ps-md-0-5'  onClick={()=>{setErr("")}}>Login</Link></li>
                         </ul>
                       )
                   }
