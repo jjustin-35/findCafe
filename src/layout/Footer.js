2fdb78c0ea7e51c550ba4d5773c67f62796237e0
jjustin-35 <85622727+jjustin-35@users.noreply.github.com>
2022-10-10 01:28:43 +0800

    chage footer

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -41,12 +41,19 @@ export const Footer = () => {
                         </ul>
                       )
                   }
-                <ul className="nav align-items-md-center flex-nowrap">
+                {/* <ul className="nav align-items-md-center flex-nowrap">
                     <li><a href="" className='nav-link px-md-auto px-0-5'><img src={`${localUrl}/img/facebook.svg`} alt="facebook" className='footer__icon'/></a></li>
                     <li><a href="" className='nav-link px-md-auto px-0-5'><img src={`${localUrl}/img/instagram.svg`} alt="instagram" className='footer__icon'/></a></li>
                     <li><a href="" className='nav-link px-md-auto px-0-5'><img src={`${localUrl}/img/line.svg`} alt="line" className='footer__icon'/></a></li>
                     <li><a href="" className='nav-link  px-md-auto px-0-5'><img src={`${localUrl}/img/email.svg`} alt="email" className='align-middle footer__icon'/></a></li>
-                </ul>
+                </ul> */}
+                <div className="navbar-brand p-0 fs-2 me-lg-5"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 40 40" fill="none" className='me-0-5 align-middle nav-logo'>
+                  <path d="M30 13.3333H31.6667C33.4348 13.3333 35.1305 14.0357 36.3807 15.2859C37.631 16.5362 38.3333 18.2319 38.3333 20C38.3333 21.7681 37.631 23.4638 36.3807 24.714C35.1305 25.9643 33.4348 26.6666 31.6667 26.6666H30" stroke="#68472B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                  <path d="M3.33325 13.3333H29.9999V28.3333C29.9999 30.1014 29.2975 31.7971 28.0473 33.0474C26.7971 34.2976 25.1014 35 23.3333 35H9.99992C8.23181 35 6.53612 34.2976 5.28587 33.0474C4.03563 31.7971 3.33325 30.1014 3.33325 28.3333V13.3333Z" stroke="#68472B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                  <path d="M10 1.66669V6.66669" stroke="#68472B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                  <path d="M16.6667 1.66669V6.66669" stroke="#68472B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                  <path d="M23.3333 1.66669V6.66669" stroke="#68472B" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                </svg><img src={`${localUrl}/img/findCafe.svg`} alt="" height={30}/></div>
               </div>
               <p className="text-white text-center mb-0">Copyright © 2022 Justin Chen. All rights reserved. </p>
           </div>
