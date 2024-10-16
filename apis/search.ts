f60840669a037753284574db52be7cc0f2ca251a
jjustin-35 <jjusttin11@gmail.com>
2024-10-17 02:04:54 +0800

    update: areas

--- apis/search.ts
+++ apis/search.ts
@@ -1,5 +1,27 @@
 'use server';
 
+import fs from 'fs';
+import path from 'path';
+import prisma from '@/lib/prisma';
+
 export const getAreas = async () => {
-    
-}
+  const allAreas = await prisma.area.findMany();
+  if (!allAreas?.length) {
+    const data = JSON.parse(fs.readFileSync(path.join(__dirname, '../taiwan_districts.json'), 'utf-8'));
+    const areas = data.map((area) => {
+      return {
+        name: area.name,
+        districts: area.districts.map((district) => {
+          return {
+            zipcode: district.zip,
+            name: district.name,
+          };
+        }),
+      };
+    });
+    await prisma.area.createMany({
+      data: areas,
+    });
+  }
+  return allAreas;
+};
--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import { hash } from 'bcrypt';
 
 const prismaClientSingleton = () => new PrismaClient();
 
