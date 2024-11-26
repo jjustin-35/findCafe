65acaa55fb90b0c7ce3cd35f5404043b6287c4c1
jjustin-35 <jjusttin11@gmail.com>
2024-11-27 01:04:47 +0800

    update: cafe list

--- components/CafeList/index.tsx
+++ components/CafeList/index.tsx
@@ -1,5 +1,16 @@
-const CafeList = () => {
-  return <></>;
+"use client";
+
+import { Stack } from "@mui/material";
+import CafeListItem, { CafeItemProps } from "./item";
+
+const CafeList = ({ data }: { data: CafeItemProps[] }) => {
+  return (
+    <Stack gap={2}>
+      {data.map((cafe) => (
+        <CafeListItem key={cafe.cafe.id} cafe={cafe} />
+      ))}
+    </Stack>
+  );
 };
 
 export default CafeList;
--- components/CafeList/item.tsx
+++ components/CafeList/item.tsx
@@ -1,38 +1,40 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import { Stack, Typography, Rating, Chip } from '@mui/material';
 import { Prisma } from '@prisma/client';
 
-interface ItemProps {
-  cafe: Prisma.CafeGetPayload<{
-    include: {
-      img: true;
-      address: true;
-      tags: true;
-    };
-  }>;
-}
+export type CafeItemProps = Prisma.CafeGetPayload<{
+  include: {
+    img: true;
+    address: true;
+    tags: true;
+  };
+}>;
 
-const CafeListItem = ({ cafe }: ItemProps) => {
-  const { img, address, tags } = cafe;
+const CafeListItem = ({ cafe }: { cafe: CafeItemProps }) => {
+  const { img, address, tags, name } = cafe;
+  const path = `/cafe/${name}`;
   const stars = Math.round(cafe.stars);
   return (
-    <Stack gap={2}>
-      <Image src={img[0].pic} alt={img[0].name} width={100} height={100} />
-      <Stack direction="column" alignItems="center" gap={1}>
-        <Typography variant="h4" component="h3" color="primary">
-          {cafe.name}
-        </Typography>
-        <Rating value={stars} readOnly />
-        <Stack direction="row" gap={1}>
-          {tags.map((tag) => (
-            <Chip key={tag.id} label={tag.name} />
-          ))}
+    <Link href={path}>
+      <Stack gap={2}>
+        <Image src={img[0].pic} alt={img[0].name} width={100} height={100} />
+        <Stack direction="column" alignItems="center" gap={1}>
+          <Typography variant="h4" component="h3" color="primary">
+            {name}
+          </Typography>
+          <Rating value={stars} readOnly />
+          <Stack direction="row" gap={1}>
+            {tags.map((tag) => (
+              <Chip key={tag.id} label={tag.name} />
+            ))}
+          </Stack>
+          <Typography variant="body1" component="p" color="text.secondary">
+            {address.location}
+          </Typography>
         </Stack>
-        <Typography variant="body1" component="p" color="text.secondary">
-          {address.location}
-        </Typography>
       </Stack>
-    </Stack>
+    </Link>
   );
 };
 
